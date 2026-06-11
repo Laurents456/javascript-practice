05e83e6e10c205fa5d4e465539beb81c4afb321e
Laurents456 <andikagendel11@gmail.com>
2026-06-11 15:03:36 +0700

    add array and function practice

---
+++ 02-array/array-basic.js
@@ -0,0 +1,5 @@
+const characters = ["Gojo", "Luffy", "Naruto", "Levi"];
+
+console.log(characters);
+console.log(characters[0]);
+console.log(`Total characters: ${characters.length}`);
---
+++ 03-function/function-basic.js
@@ -0,0 +1,6 @@
+function introduce(name, skill) {
+  return `Hi, I'm ${name} and I'm learning ${skill}.`;
+}
+
+console.log(introduce("Laurents", "JavaScript"));
+
